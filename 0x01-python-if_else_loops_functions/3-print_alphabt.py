b98c4d33787345651c5289dc5b284e75f8aad515
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-08 20:32:46 +0100

    ommit e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    print('{}'.format(chr(letter)), end="")
+    if letter != ord('q') and letter != ord('e'):
+        print('{}'.format(chr(letter)), end="")
