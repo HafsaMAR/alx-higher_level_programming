3685aae5027b4060a47ce334d4133cf5ab46c626
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-08 23:17:28 +0100

    smallest digit combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ten in range(9):
-    for unit in range(ten + 1, 9):
+    for unit in range(ten + 1, 10):
         print("{}{}".format(ten, unit), end=', ')
 print("{}".format(89))
