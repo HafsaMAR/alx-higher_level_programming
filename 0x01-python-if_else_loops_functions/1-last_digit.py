6321fd0310cdacab71022652db8227e402dc1e75
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-08 19:45:04 +0100

    indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,12 +5,12 @@ Last_digit = number
 if Last_digit % 10 == 0:
     print("Last digit of {} is 0 and is 0".format(number))
 elif Last_digit < 0:
-    print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format
-        (number, abs(Last_digit) % 10))
+    print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(
+        number, abs(Last_digit) % 10))
 elif abs(Last_digit % 10) < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
-        (number, Last_digit % 10))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
+        number, Last_digit % 10))
 else:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number,
-        Last_digit % 10))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(
+        number, Last_digit % 10))
 
