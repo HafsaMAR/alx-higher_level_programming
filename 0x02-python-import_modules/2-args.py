c6453ef2d47ac4991b086cb5d3f26bd3fdf1aeef
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-11 15:30:40 +0100

    dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+num_arguments = len(sys.argv)
+if num_arguments == 1:
+    print("0 arguments.")
+elif num_arguments == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(num_arguments))
+    for i in range(1, num_arguments):
+        print("{}: {}".format(i, sys.argv[i]))
