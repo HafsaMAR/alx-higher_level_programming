3df4f887a565ae8ade00cb98c0ccd56eb39a96e6
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-11 22:44:23 +0100

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if sys.argv[2] not in ('+', '-', '*', '/'):
+            print ('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
+        else:
+            a = sys.argv[1]
+            b = sys.argv[3]
+            if sys.argv[2] == "+":
+                print("{} + {} = {}".format(a, b, add(a, b)))
+            elif sys.argv[2] == "-":
+                print("{} - {} = {}".format(a, b, sub(a, b)))
+            elif sys.argv[2] == "*":
+                print("{} * {} = {}".format(a, b, mul(a, b)))
+            else:
+                print("{} / {} = {}".format(a, b, div(a, b)))
