63a3d1589a5c83c21739319382b3005cae59238c
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-11 16:46:49 +0100

    infinit addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import sys
-if __name__ == __"main__":
+
+if __name__ == "__main__":
+    import sys
     if len(sys.argv) == 0:
         print(0)
     else:
         result = 0
         for i in range(1, len(sys.argv)):
             result += int(sys.argv[i])
-            print("{}".format(result))
+        print("{}".format(result))
