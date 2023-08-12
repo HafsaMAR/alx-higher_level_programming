25881fcec291b3e3874a561a357cfd2d4d5d246a
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-12 19:28:10 +0100

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/pyhton3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
+    if 0 <= idx < len(my_list):
         my_list[idx] = element
         return my_list
