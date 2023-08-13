47bb44ed8d6afae32d7543dbbf67e3b2bf517431
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-13 17:07:52 +0100

    Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+int is_palindrome(listint_t **head)
+{
+    int n = 0, i = 0, j = 0;
+    listint_t *half_2_node, *half_1_node, *current = *head;
+    half_1_node = *head;
+    half_2_node = *head;
+
+    while (current)
+    {
+        current = current->next;
+        n++;
+    }
+    while (j < n / 2)
+    {
+        half_2_node = half_2_node->next;
+        j++;
+    }
+    j = 0;
+    while (n / 2 + j > n && half_2_node)
+    {
+        half_2_node = half_2_node->next;
+        while (i < n / 2 - j)
+        {
+            half_1_node = half_1_node->next;
+        }
+        if (half_2_node->n != half_1_node->n)
+        {
+            return (0);
+        }
+        j++;
+    }
+    return (1);
+}
