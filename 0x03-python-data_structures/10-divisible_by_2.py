8bb6bddd703118555f77e64955dad233f818f3d8
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-13 14:48:50 +0100

    even number in list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    if my_list:        
+    if my_list:
         for num in my_list:
             if num % 2 == 0:
                 new_list.append(True)
