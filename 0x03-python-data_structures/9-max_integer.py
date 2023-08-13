f60b0778b63c681f14fda535755c3c04820539a1
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-13 14:33:27 +0100

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list:
+        num = my_list[0]
+        for i  in range(1, len(my_list)):
+            if num < my_list[i]:
+                num = my_list[i]
+        return num
+    else:
+        return None
