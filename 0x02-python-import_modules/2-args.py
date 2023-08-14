e539d0db5bd361d55f46311b563986604ae6c8e6
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-14 07:01:37 +0100

    requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     elif num_arguments == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-        else:
+    else:
             print("{} arguments:".format(num_arguments - 1))
             for i in range(1, num_arguments):
                 print("{}: {}".format(i, sys.argv[i]))
