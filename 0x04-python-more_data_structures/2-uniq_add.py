ea5027b2495df0ae8f6ff40a5ab81eb16525eaeb
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-16 21:40:08 +0100

    requirement

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,5 +2,5 @@
 
 def uniq_add(my_list=[]):
     unique_list = set()
-    return sum(map(lambda x: x if x not in unique_list and not unique_list.add(x) else 0, my_list))
-    
+    return sum(map(lambda x: x if x not in unique_list and not unique_list.add(
+        x) else 0, my_list))
