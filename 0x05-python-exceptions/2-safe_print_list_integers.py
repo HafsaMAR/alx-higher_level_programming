0309bb705115853bf00f015c16cc41f8200a261d
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-28 23:20:34 +0100

    raise exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             counter += 1
         except (ValueError, TypeError):
             counter += 1
+        except IndexError:
+            raise
     print()
     return counter
