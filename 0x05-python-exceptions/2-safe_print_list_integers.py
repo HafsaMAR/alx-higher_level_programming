5f267a85c04b849d53a3f6df86dfbfb47b085217
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-28 23:19:37 +0100

    exception_python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for counter in range(x):
+        try:
+            print("{:d}".format(my_list[counter]), end='')
+            counter += 1
+        except (ValueError, TypeError):
+            counter += 1
+    print()
+    return counter
