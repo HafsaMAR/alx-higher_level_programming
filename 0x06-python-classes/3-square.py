ed4471c4c864306f50b71d65d6b46ac09d22aa1f
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-29 23:37:56 +0100

    Square Area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""This module defines a class Square"""
+
+class Square:
+    """  A class that Create the private instance attribute size"""
+
+    def __init__(self, size=0):
+        """Initialize the size of the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return self.__size
