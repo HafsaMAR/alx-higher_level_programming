9a55b491036c179d193d6ba74466494596348885
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-29 23:45:09 +0100

    setter and getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/pyton3
+
+"""This module defines a class Square"""
+
+class Square:
+    """  A class that Create the private instance attribute size"""
+    
+    def __init__(self, size=0):
+        """Initialize the size of the square"""
+        self.__size = size
+        @property
+        def size(self):
+            return self.__size
+        @size.setter
+        def size(self, value):
+            
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+    
+    def area(self):
+        return self.__size**2
