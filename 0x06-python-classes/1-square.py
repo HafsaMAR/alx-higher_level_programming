e06b6ff8691ebf2f4790dfea631ca3b481c8b619
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-29 22:35:50 +0100

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+"""This module creates a class Square"""
+
+class Square:
+    """ Create the private instance attribute size"""
+    def __init__(self, size):
+        self.size = size
