be067be53c714895869d2a093e0f570ea47846d5
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-30 22:33:35 +0100

    setter and getter private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,20 +10,20 @@ class Square:
         """Initialize the size of the square"""
         self.__size = size
 
-        @property
-        def size(self):
-            """Public instance method that returns the current square size"""
+    @property
+    def size(self):
+        """Public instance method that returns the current square size"""
             
-            return self.__size
+        return self.__size
         
-        @size.setter
-        def size(self, value):
+    @size.setter
+    def size(self, value):
             
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
     
     def area(self):
         return self.__size**2
