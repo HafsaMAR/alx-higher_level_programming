810038bb86cee0714f1c20e195c4ec24c135e4dd
HafsaMAR <marouche.hafsa@gmail.com>
2023-08-30 22:35:24 +0100

    pycodestyle

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,7 +5,7 @@
 
 class Square:
     """  A class that Create the private instance attribute size"""
-    
+
     def __init__(self, size=0):
         """Initialize the size of the square"""
         self.__size = size
@@ -13,17 +13,17 @@ class Square:
     @property
     def size(self):
         """Public instance method that returns the current square size"""
-            
+
         return self.__size
-        
+
     @size.setter
     def size(self, value):
-            
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size**2
