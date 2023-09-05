a13bf54957fd24788b359d3c32b2b60f09e7a10e
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-05 20:41:51 +0100

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Myclass:
-    func_time_call = 0
+    func_time_call = -1
 def magic_string():
     Myclass.func_time_call +=1
-    return "BestSchool" * Myclass.func_time_call
+    return "BestSchool, " * Myclass.func_time_call + "BestSchool"
