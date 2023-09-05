c637ad4178786e03c90c0a9490acfdd1bb4b4e89
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-05 20:51:31 +0100

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.func_time_call = getattr(magic_string, "func_time_call", 0) + 1
-    return ", ".join(["BestSchool" for _ in range(magic_string.func_time_call)])
+    magic_string.func_time_call = getattr(
+        magic_string, "func_time_call", 0) + 1
+    return ", ".join(["BestSchool" for _ in range(
+        magic_string.func_time_call)])
