9e199a959c5e55ac07e8b45d8d24bb59ad98ad43
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-11 23:21:00 +0100

    pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,6 +3,7 @@
 """Module for rectange class that inherits from BaseGeometry"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from baseGeometry superclass
     """
@@ -20,4 +21,3 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-
