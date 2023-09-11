a4b202dbe00b7d4853b5e095460ca187c8dfe938
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-11 22:06:46 +0100

    improve Geometry _ raise exception in class method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Module of BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class in which area methode is defined"""
+    def area(self):
+        """Public instance method
+        Args:
+        self: this class
+
+        Raise:
+            exception
+        """
+        raise Exception('area() is not implemented')
