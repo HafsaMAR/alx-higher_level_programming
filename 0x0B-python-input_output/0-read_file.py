4564d3377f4f600b8cedb818f8f360c5e9960e72
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-12 22:09:41 +0100

    print the content of file with no newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,5 +12,4 @@ def read_file(filename=""):
         None
     """
     with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data)
+        print(f.read, end="")
