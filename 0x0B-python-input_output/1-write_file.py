20bf534b42d8abd72978cb4d498676b1304d4a7d
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-12 16:45:09 +0100

    Implement write_file function

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Module for wirte_file function"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function that write into a file
+    and returns the number of characters written
+
+    Args:
+        filename: file object to write into
+        text: string to be writen
+
+    Return: number if characters written
+    """
+    with open(filename, "w", encoding="UTF8") as f:
+        f.write(text)
+        return f.seek(0, 2)
