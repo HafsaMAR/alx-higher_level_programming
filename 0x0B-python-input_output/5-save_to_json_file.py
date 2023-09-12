47a144dc49cfcb4fc9a718f3a953398ac8de3acb
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-12 23:55:36 +0100

    arg list to json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -15,4 +15,4 @@ def save_to_json_file(my_obj, filename):
     """
     import json
     with open(filename, "w", encoding="UTF8") as f:
-        f.write(json.dumps(my_obj))
+        json.dumps(my_obj, f)
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for a script that adds the arguments
+to a pyhton list and save them in a file"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
+
+def add_item(arg):
+    """adds all arguments passed to list and write it to json file
+
+    Args:
+        arg: argment passed
+    """
+    filename = "add_item.json"
+    arg_list = load_from_json_file(filename)
+    """try:
+        
+    except FileNotFoundError:
+        arg_list = []
+"""
+    for i in range(1, len(arg)):
+        arg_list.append(arg[i])
+        save_to_json_file(arg_list, filename)
+
+if __name__ == "__main__":
+    add_item(sys.argv)
