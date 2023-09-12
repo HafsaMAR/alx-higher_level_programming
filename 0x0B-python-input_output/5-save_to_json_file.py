ec42ac10bf480f491034e62f2a34a7bcc9dcaf4f
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-12 22:38:49 +0100

    write a JSON represenation into file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module for save_to_json function"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Function that write JSON representation into a file
+
+    my_obj: python data structure
+    filename: file
+
+    Returns:
+        None
+    """
+    import json
+    with open(filename, "w", encoding="UTF8") as f:
+        f.write(json.dumps(my_obj))
