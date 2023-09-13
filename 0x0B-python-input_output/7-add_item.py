ed555e4df7baf1b18b38651effd37a3b28ac2e3d
HafsaMAR <marouche.hafsa@gmail.com>
2023-09-13 17:01:43 +0100

    update add_item function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,10 @@
 """Module for a script that adds the arguments
 to a pyhton list and save them in a file"""
 
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
+
 
 def add_item(arg):
     """adds all arguments passed to list and write it to json file
@@ -15,13 +16,14 @@ def add_item(arg):
     filename = "add_item.json"
     try:
         arg_list = load_from_json_file(filename)
-    
+
     except FileNotFoundError:
         arg_list = []
 
     for i in range(1, len(arg)):
         arg_list.append(arg[i])
         save_to_json_file(arg_list, filename)
+
 
 if __name__ == "__main__":
     add_item(sys.argv)
