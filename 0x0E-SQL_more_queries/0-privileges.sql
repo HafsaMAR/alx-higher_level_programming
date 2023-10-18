03f4dc22ea55bc2c4a3c41199fe2d7d8417b3f75
HafsaMAR <marouche.hafsa@gmail.com>
2023-10-18 20:10:37 +0100

    privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+
+-- Use the information_schema database
+USE information_schema;
+
+-- List all privileges of user_0d_1 on localhost
+SELECT * FROM user_privileges WHERE grantee = "'user_0d_1'@'localhost'";
+
+-- List all privileges of user_0d_2 on localhost
+SELECT * FROM user_privileges WHERE grantee = "'user_0d_2'@'localhost'";
