0c89ff3534dc5f43a416f295e2a4e4500c4bf231
HafsaMAR <marouche.hafsa@gmail.com>
2023-10-18 21:59:57 +0100

    create database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE database IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grant select privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
