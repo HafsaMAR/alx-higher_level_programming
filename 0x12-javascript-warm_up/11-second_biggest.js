7e9e6ab6ced3ed878237e54d7170b110e6e2ab72
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-14 00:13:54 +0100

    Js warmup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-function secondBiggest(args) {
-    if (args.length === 0) {
-      console.log(0);
-    } else if (args.length === 1) {
-      console.log(0);
-    } else {
-      let max = -Infinity;
-      let secondMax = -Infinity;
-      for (let i = 0; i < args.length; i++) {
-        const num = parseInt(args[i]);
-        if (num > max) {
-          secondMax = max;
-          max = num;
-        } else if (num > secondMax) {
-          secondMax = num;
-        }
+function secondBiggest (args) {
+  if (args.length === 0) {
+    console.log(0);
+  } else if (args.length === 1) {
+    console.log(0);
+  } else {
+    let max = -Infinity;
+    let secondMax = -Infinity;
+    for (let i = 0; i < args.length; i++) {
+      const num = parseInt(args[i]);
+      if (num > max) {
+        secondMax = max;
+        max = num;
+      } else if (num > secondMax) {
+        secondMax = num;
       }
-      console.log(secondMax);
     }
+    console.log(secondMax);
   }
-let args = process.argv.slice(2).map(Number);
-secondBiggest(args)
+}
+const args = process.argv.slice(2).map(Number);
+secondBiggest(args);
