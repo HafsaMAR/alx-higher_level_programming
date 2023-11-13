a67c16871ee0a5390ddfb043364e98a1f08402e8
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-13 23:25:20 +0100

    Js warmup

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv.length > 2 && Number.isInteger(Number(process.argv[2]))) {
   console.log(`My number: ${parseInt(process.argv[2])}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
