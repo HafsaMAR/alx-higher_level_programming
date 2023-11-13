c3ecfbf69bf3aab3f48a6b861b6077937ad9132d
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-13 23:35:08 +0100

    Js warmup

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const x = Number(process.argv[2]);
+
+if (Number.isInteger(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
