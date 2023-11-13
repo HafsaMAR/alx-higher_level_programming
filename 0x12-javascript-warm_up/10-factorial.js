344fad54e0b0ef1694a7336930cc53fcee42b227
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-13 23:58:18 +0100

    Js warmup

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   } else if (n <= 1) {
@@ -7,6 +7,6 @@ function factorial(n) {
   } else {
     return factorial(n - 1) * n;
   }
-};
+}
 
 console.log(factorial(Number(process.argv[2])));
