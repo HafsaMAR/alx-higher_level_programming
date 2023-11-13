0209117b977e0ee05f98a742b88d6d213adbc6b8
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-13 23:40:48 +0100

    Js warmup

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const x = Number(process.argv[2]);
+
+if (Number.isInteger(x)) {
+  let square = '';
+  for (let i = 0; i < x - 1; i++) {
+    square += 'X'.repeat(x) + '\n';
+  }
+  square += 'X'.repeat(x)
+  console.log(square);
+} else {
+  console.log('Missing size');
+}
