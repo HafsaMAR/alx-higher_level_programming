23f46453b05906f99d533bba1fcf336f407294af
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-14 23:31:04 +0100

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.Width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.Width && this.height){
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.Width));
+    }
+  }}
+
+  rotate () {
+    if (this.Width && this.height){
+        [this.Width, this.height] = [this.height, this.Width]
+    }
+  }
+
+  double () {
+    if (this.Width && this.height) {
+        [this.Width, this.height] = [this.Width * 2, this.height * 2]
+    }
+  }
+}
+module.exports = Rectangle;
