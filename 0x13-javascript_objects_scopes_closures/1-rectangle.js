18906c69658df1a89d253c196b3d90837c583430
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-14 22:52:19 +0100

    first commit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (h,w) {
-        this.height = h;
-        this.width = w;
-    }
+  constructor (h, w) {
+    this.height = h;
+    this.width = w;
+  }
 }
 module.exports = Rectangle;
