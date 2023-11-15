50ea09277d957b1dea842cb28bb336d140bed20c
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-15 14:48:19 +0100

    semistandard validation

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reversedList = [];
-  for (let i = list.lenght - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
   return reversedList;
