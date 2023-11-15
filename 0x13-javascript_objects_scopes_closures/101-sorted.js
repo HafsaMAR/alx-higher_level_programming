e3a6d468b23dc0d0b2b6706905ccee9fe30703a2
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-15 18:35:35 +0100

    work on imported dictionary

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const { dict }  = require('./101-data')
+const occurences = {};
+for (const userId in dict) {
+    const occurence = dict[userId];
+    if (occurences[occurence] === undefined) {
+        occurences[occurence] = [userId];
+    }
+    else {
+        occurences[occurence].push(userId);
+    }
+}
+console.log(occurences);
