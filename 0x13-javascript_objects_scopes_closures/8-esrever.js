5a05cde6b4b5fd187a11cb6fd614731cac0f3731
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-15 14:14:46 +0100

    reverse without reverse method

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+    let reversedList = [];
+    for (let i = list.lenght - 1; i >= 0; i--) {
+        reversedList.push(list[i])        
+    }
+    return reversedList
+}
