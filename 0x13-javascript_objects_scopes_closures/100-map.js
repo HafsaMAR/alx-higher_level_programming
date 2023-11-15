6b7474b776fdf09f0eed4ea2eee1dff9cb16ae3c
HafsaMAR <marouche.hafsa@gmail.com>
2023-11-15 17:17:57 +0100

    map faxtor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 newlist = list.map((element, index, list) => element * index);
-console.log(list)
+console.log(list);
 console.log(newlist);
-
