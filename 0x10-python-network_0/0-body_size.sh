24da98d8ed9749ace92860b08e34a1db3b460110
HafsaMAR <marouche.hafsa@gmail.com>
2023-12-01 01:06:04 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,9 +3,6 @@
 url=$1
 
 #Send request to URL and save response to body variable
-response=$(curl -sI "$url" | awk '/Content-Length/ {print $2}' | tr -d '\r')
+curl -sI "$url" | grep Content-Length | cut -d' ' -f2
 
-#Get size of response body bytes
-size=$(echo -n "$response" | wc -c)
 
-echo "$size"
