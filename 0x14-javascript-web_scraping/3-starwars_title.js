88453fa4e2a18d17c3780d90ea64f917fe838cf3
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 00:57:26 +0100

    get the title of the ep

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, function (_error, { response }, body) {
+  const result = JSON.parse(body);
+
+  console.log(result.title);
+});
