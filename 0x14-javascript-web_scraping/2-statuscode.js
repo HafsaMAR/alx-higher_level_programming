12a008aa3ce3574f8d17708d661b9c7fe6667b28
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 00:47:56 +0100

    semistandard requirement

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (_error, { statusCode }) {
-  console.log(statusCode);
+  console.log(`code: ${statusCode}`);
 });
