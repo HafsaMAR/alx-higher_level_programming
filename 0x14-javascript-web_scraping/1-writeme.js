6f0370067ec79478d799049832b66b7bcf81832a
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 00:36:17 +0100

    semistandard requirement

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const writeFile = require('fs').writeFile;
+
+const fileName = process.argv[2];
+const string = process.argv[3];
+
+writeFile(fileName, string, function () {
+  console.log('SUCCESS');
+});
