82b9fad08b89c8da723ed6517be7dd4571b666aa
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 01:42:02 +0100

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,20 +8,16 @@ request(url, function (_error, response, rawBody) {
   const body = JSON.parse(rawBody);
   const films = body.results;
 
-  const characters = films.map(function (film) {
-    return film.characters
-      .map(
-        function (url) {
-          return url === 'https://swapi-api.alx-tools.com/api/people/18/';
-        }
-      )
-      .filter(function (element) { return element; }).length;
-  });
+  let count = 0;
+  for (const film of films) {
+    const characters = film.characters;
 
-  console.log(
-    characters.reduce(
-      function (acc, curr) { return acc + curr; },
-      0
-    )
-  );
+    for (const character of characters) {
+      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count++;
+      }
+    }
+  }
+
+  console.log(count);
 });
