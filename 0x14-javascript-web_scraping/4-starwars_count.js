78ca4fcf2c3e54ab9fffea2b97815c627d8bf36d
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 01:30:45 +0100

    number of matching ID

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (_error, response, rawBody) {
+  const body = JSON.parse(rawBody);
+  const films = body.results;
+
+  const characters = films.map(function (film) {
+    return film.characters
+      .map(
+        function (url) {
+          return url === 'https://swapi-api.alx-tools.com/api/people/18/';
+        }
+      )
+      .filter(function (element) { return element; }).length;
+  });
+
+  console.log(
+    characters.reduce(
+      function (acc, curr) { return acc + curr; },
+      0
+    )
+  );
+});
