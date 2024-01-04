2cd75bfe7983f7ae0d9b24bfa3f7a61a2a0497fa
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 01:56:50 +0100

    write to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const { writeFile } = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, function (error, _response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  writeFile(path, body, () => {})
+});
