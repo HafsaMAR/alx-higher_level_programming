85ad211f47619654b9f71cd9b503929beccb6353
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-04 02:28:02 +0100

    Retrieve and Parse JSON Data from an API Endpoint

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, _response, rawBody) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const tasks = JSON.parse(rawBody);
+
+  const result = {};
+
+  for (const task of tasks) {
+    const id = task.userId;
+
+    if (task.completed) {
+      result[id] = (result[id] ?? 0) + 1;
+    }
+  }
+
+  console.log(result);
+});
